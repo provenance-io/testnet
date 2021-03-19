b7572ddc2ded5ab1f191c200f88cb66813ae0576
Phil Story <mtps@users.noreply.github.com>
2021-03-18 19:12:10 -0600

    Add docker build files for testnet-beta and pio-testnet-1 archive and visor nodes

---
+++ docker/archive/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ ! -f "${PIO_HOME}/config/genesis.json" ]; then
+    mkdir -p "${PIO_HOME}/config"
+    cp "/${CHAIN_ID}/genesis.json" "${PIO_HOME}/config/genesis.json"
+    cp "/${CHAIN_ID}/config.toml" "${PIO_HOME}/config/config.toml"
+fi
+
+if [ ! -d "${PIO_HOME}/data" ]; then
+    mkdir "${PIO_HOME}/data"
+fi
+
+exec "$@"
